d9605ce8e881b286ccc520b9d85e231c6ec8fe5a
Mamadou Siradiou Diallo <mamadousiradioudiallo4@gmail.com>
2024-01-11 18:28:02 +0000

    fix bug with app launching

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,12 @@ module com.quiz_mongodb {
     requires javafx.controls;
     requires javafx.fxml;
 
+    requires javafx.graphics;
+    requires javafx.base;
     requires org.kordamp.ikonli.javafx;
     requires org.kordamp.bootstrapfx.core;
+
+    opens com.quiz_mongodb to javafx.fxml;
+    exports com.quiz_mongodb;
+    exports com.quiz_mongodb.controller;
 }
