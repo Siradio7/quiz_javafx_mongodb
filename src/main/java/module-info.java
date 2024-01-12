4691dc74b70c1a2113b6c8b6d7343456e285e089
Mamadou Siradiou Diallo <mamadousiradioudiallo4@gmail.com>
2024-01-12 19:25:35 +0000

    updating dependencies

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -15,4 +15,6 @@ module com.quiz_mongodb {
     exports com.quiz_mongodb;
     opens com.quiz_mongodb.controller to javafx.fxml;
     exports com.quiz_mongodb.controller;
+    exports com.quiz_mongodb.database;
+    opens com.quiz_mongodb.database to javafx.fxml;
 }
