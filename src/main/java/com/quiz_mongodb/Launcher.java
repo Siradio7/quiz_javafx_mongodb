c42adbcd1614d1ed6dba49e89bb7cc3811189c6f
Mamadou Siradiou Diallo <mamadousiradioudiallo4@gmail.com>
2024-01-17 13:49:48 +0000

    adding launcher for the future jar tool

---
+++ src/main/java/com/quiz_mongodb/Launcher.java
@@ -0,0 +1,10 @@
+package com.quiz_mongodb;
+
+import com.quiz_mongodb.App;
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(App.class, args);
+    }
+}
